f0a24416019bb364d53abf23c6a9ceff7fb86ffd
0xWindsor <50457850+0xWindsor@users.noreply.github.com>
2023-04-24 14:27:54 +0300

    prompt selection bug fix

--- components/categories/categoryList.jsx
+++ components/categories/categoryList.jsx
@@ -1,49 +1,84 @@
+"use client";
+import { useState } from "react";
 export default function CategoryList(props) {
+  const { category, setPrompt, setNegativePrompt } = props;
 
   var earlyPrompt = {};
   var earlyNegativePrompt = {};
 
-  function handle(e) {
-    earlyPrompt[e.target.getAttribute('data-sub-category')] = e.target.getAttribute('data-prompt');
-    earlyNegativePrompt[e.target.getAttribute('data-sub-category')] = e.target.getAttribute('data-negative-prompt');
+  const subCategoryKeys = Object.keys(category);
 
-    props.setPrompt(earlyPrompt);
-    props.setNegativePrompt(earlyNegativePrompt);
+  const initialState = subCategoryKeys.reduce((obj, item) => {
+    obj[item] = null;
+    return obj;
+  }, {});
+
+  const [selectedLooks, setSelectedLooks] = useState(initialState);
+
+  function handle(e) {
+    const subCategory = e.target.getAttribute("data-sub-category");
+    setSelectedLooks((prevState) => {
+      return {
+        ...prevState,
+        [subCategory]: {
+          name: e.target.alt,
+          prompt: e.target.getAttribute("data-prompt"),
+          negativePrompt: e.target.getAttribute("data-negative-prompt"),
+        },
+      };
+    });
+
+    setPrompt(selectedLooks);
   }
 
-  const returnCategory = (category) => {
-    category = Object.entries(category);  //[subCategory(Array), subCategory(Array)]
+  const returnCategory = () => {
+    let categoryEntries = Object.entries(category); //[subCategory(Array), subCategory(Array)]
 
     return (
       <div>
-        {category ? category.map((c, key) => //['subCategoryName', subCategoryItems(Array)]
-          <div key={key}>
+        {categoryEntries
+          ? categoryEntries.map(
+              (
+                c,
+                key //['subCategoryName', subCategoryItems(Array)]
+              ) => (
+                <div key={key}>
+                  {/* Sub Menu Button */}
+                  <div
+                    className="cursor-default flex items-center justify-center p-4 my-8 text-white text-center w-full rounded-lg bg-opacity-30 bg-white"
+                    key={c[0]}
+                  >
+                    {c[0]}
+                  </div>
 
-            {/* Sub Menu Button */}
-            <div className="cursor-default flex items-center justify-center p-4 my-8 text-white text-center w-full rounded-lg bg-opacity-30 bg-white"
-              key={c[0]}>
-              {c[0]}</div>
-
-            {/* Sub Menu Items*/}
-            <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-4">
-              {c[1].map((e, key) => //{"name": "Name", "image": "url", "prompt": "prompt", "negativePrompt": "negative prompt"}
-                <img className="rounded-lg h-24 cursor-pointer"
-                  key={key}
-                  alt={e.name}
-                  src={e.image}
-                  data-sub-category={c[0]}
-                  data-prompt={e.prompt}
-                  data-negative-prompt={e.negativePrompt}
-                  onClick={e => handle(e)} tabIndex={key}></img>)}
-            </div>
-
-          </div>
-        ) : null}
+                  {/* Sub Menu Items*/}
+                  <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-4">
+                    {c[1].map(
+                      (
+                        e,
+                        key //{"name": "Name", "image": "url", "prompt": "prompt", "negativePrompt": "negative prompt"}
+                      ) => (
+                        <img
+                          className="rounded-lg h-24 cursor-pointer"
+                          key={key}
+                          alt={e.name}
+                          src={e.image}
+                          data-sub-category={c[0]}
+                          data-prompt={e.prompt}
+                          data-negative-prompt={e.negativePrompt}
+                          onClick={(e) => handle(e)}
+                          tabIndex={key}
+                        ></img>
+                      )
+                    )}
+                  </div>
+                </div>
+              )
+            )
+          : null}
       </div>
-    )
-  }
+    );
+  };
 
-  return (
-    returnCategory(props.category)
-  )
+  return returnCategory();
 }
